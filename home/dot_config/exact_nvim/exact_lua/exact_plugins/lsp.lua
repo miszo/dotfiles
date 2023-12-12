329f9827a9ab17436f000f2c57760532937961f6
Miszo Radomski <hello@miszo.dev>
2023-12-12 16:35:09 +0100

    Add setup viable for both mono and polyrepo

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/lsp.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/lsp.lua
@@ -1,6 +1,6 @@
 local get_root_dir = function(fname)
   local util = require('lspconfig.util')
-  return util.root_pattern('.git')(fname) or util.root_pattern('package.json', 'tsconfig.json')(fname)
+  return util.root_pattern('package.json', 'tsconfig.json')(fname) or util.root_pattern('.git')(fname)
 end
 
 return {
