dc39b4e2cf91ccdbf4895b51ab54cd5d39e7500d
Miszo Radomski <hello@miszo.dev>
2024-02-05 13:58:47 +0100

    feat: show battery status always

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/ui.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/ui.lua
@@ -108,8 +108,8 @@ return {
       require('battery').setup({
         update_rate_seconds = 60, -- Number of seconds between checking battery status
         show_status_when_no_battery = false, -- Don't show any icon or text when no battery found (desktop for example)
-        show_plugged_icon = false, -- If true show a cable icon alongside the battery icon when plugged in
-        show_unplugged_icon = false, -- When true show a diconnected cable icon when not plugged in
+        show_plugged_icon = true, -- If true show a cable icon alongside the battery icon when plugged in
+        show_unplugged_icon = true, -- When true show a diconnected cable icon when not plugged in
         show_percent = true, -- Whether or not to show the percent charge remaining in digits
         vertical_icons = true, -- When true icons are vertical, otherwise shows horizontal battery icon
         multiple_battery_selection = 1, -- Which battery to choose when multiple found. "max" or "maximum", "min" or "minimum" or a number to pick the nth battery found (currently linux acpi only)
@@ -124,11 +124,6 @@ return {
     config = function(_, opts)
       local battery = {
         function()
-          local b = require('battery')
-          local status = b.get_battery_status()
-          if status.ac_power then
-            return ''
-          end
           return require('battery').get_status_line()
         end,
       }
