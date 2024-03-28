f58f4a4923d8972addce3e8b9cec98f2aed2e3cd
Miszo Radomski <hello@miszo.dev>
2024-03-28 20:50:38 +0100

    chore: remove custom scripts and download files from github

--- home/dot_config/zsh/dot_zsh_tools.zsh
+++ home/dot_config/zsh/dot_zsh_tools.zsh
@@ -9,6 +9,14 @@ source ${HOME}/.config/op/plugins.sh
 
 source ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
+#fast-syntax-highlightinh theme
+fast-theme XDG:catppuccin-mocha
+
+# fzf default options
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
 if type brew &>/dev/null; then
   FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
