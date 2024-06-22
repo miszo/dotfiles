c21178d47120a3abb435cf33de8896b96f321414
Miszo Radomski <hello@miszo.dev>
2024-06-23 00:44:14 +0200

    chore: add mising utils

---
+++ home/dot_config/exact_nvim/exact_lua/exact_utils/core.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+local supported_images = { 'png', 'jpg', 'jpeg', 'gif', 'webp', 'avif', 'heic' }
+
+M.get_hijack_images_patterns = function()
+  local patterns = {}
+
+  for _, extension in ipairs(supported_images) do
+    table.insert(patterns, string.format('*.%s', extension))
+  end
+
+  return patterns
+end
+
+M.get_extension = function(filepath)
+  local split_path = vim.split(filepath:lower(), '.', { plain = true })
+  return split_path[#split_path]
+end
+
+M.is_supported_image = function(filepath)
+  local extension = M.get_extension(filepath)
+  return vim.tbl_contains(supported_images, extension)
+end
+
+return M
