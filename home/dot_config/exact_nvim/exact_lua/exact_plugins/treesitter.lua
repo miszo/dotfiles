5f163a17fffcbdfc27c09407ffe08877be5d05a4
Miszo Radomski <hello@miszo.dev>
2024-07-09 13:50:48 +0200

    feat: add more exported settings

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/treesitter.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/treesitter.lua
@@ -90,9 +90,5 @@ return {
   -- Chezmoi syntax highlighting
   {
     'alker0/chezmoi.vim',
-    init = function()
-      vim.g['chezmoi#use_tmp_buffer'] = 1
-      vim.g['chezmoi#source_dir_path'] = os.getenv('HOME') .. '/.local/share/chezmoi'
-    end,
   },
 }
