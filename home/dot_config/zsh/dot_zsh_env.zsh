b9685eb25638149a70f2e4130a4a6b989293e29d
Miszo Radomski <hello@miszo.dev>
2025-02-06 13:04:13 +0100

    chore(asdf): add asdf envs

--- home/dot_config/zsh/dot_zsh_env.zsh
+++ home/dot_config/zsh/dot_zsh_env.zsh
@@ -1,3 +1,4 @@
+# add local clis to path
 export PATH="$PATH:/Users/miszo/.local/bin"
 # pnpm
 export PNPM_HOME="/Users/miszo/Library/pnpm"
@@ -8,6 +9,11 @@ esac
 # pnpm end
 # ripgrep config
 export RIPGREP_CONFIG_PATH=$HOME/.config/ripgrep/config
+# ripgrep config end
+# asdf
+export ASDF_DATA_DIR="$HOME/.asdf"
+export PATH="$ASDF_DATA_DIR/shims:$PATH"
+# asdf end
 export HOMEBREW_NO_AUTO_UPDATE=1
 export SSH_AUTH_SOCK=$HOME/.1password/agent.sock
 COMPLETION_WAITING_DOTS=true
