866fc485098ef81fe9f294e7fafbb9da8aa4c4b9
Miszo Radomski <hello@miszo.dev>
2025-03-05 11:46:21 +0100

    chore: remove the octo nvim plugin

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/git.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/git.lua
@@ -1,19 +1,3 @@
-local get_gh_env = function()
-  local github_token = os.getenv('OCTO_GH_TOKEN')
-  if not github_token then
-    error('Failed to get GitHub token.')
-  end
-
-  local start = string.find(github_token, 'gho_')
-  if not start == 1 then
-    error('Wrong GitHub token format. Please use a personal access token.')
-  end
-
-  return {
-    GITHUB_TOKEN = github_token,
-  }
-end
-
 ---@module "lazy"
 ---@type LazySpec[]
 return {
@@ -36,13 +20,6 @@ return {
         ':Gitsigns toggle_current_line_blame<CR>',
         { desc = 'Toggle current line blame' }
       )
-    end,
-  },
-  -- Octo - GitHub PRs and issues
-  {
-    'pwntester/octo.nvim',
-    opts = function(_, opts)
-      opts.gh_env = get_gh_env()
     end,
   },
   -- Generate link to file in the repository
