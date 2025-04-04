80e2d6eb245d00a4851cd44101bd6d4af8793089
Miszo Radomski <hello@miszo.dev>
2025-04-04 19:37:40 +0200

    chore: add support for both lsp and prettier formatting with conform

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/conform.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/conform.lua
@@ -1,12 +1,19 @@
+local prettier_with_lsp = { 'prettier', lsp_format = 'first' }
+
 ---@module "lazy"
 ---@type LazySpec[]
 return {
   {
     'stevearc/conform.nvim',
+    ---@type conform.setupOpts
     opts = {
       formatters_by_ft = {
         ruby = { vim.g.lazyvim_ruby_formatter },
         eruby = { 'prettier' },
+        javascript = prettier_with_lsp,
+        javascriptreact = prettier_with_lsp,
+        typescript = prettier_with_lsp,
+        typescriptreact = prettier_with_lsp,
       },
     },
   },
