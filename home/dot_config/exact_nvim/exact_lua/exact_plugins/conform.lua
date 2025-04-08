d2e62f6fa62e986b6e7db73693b5228e1fb12187
Miszo Radomski <hello@miszo.dev>
2025-04-08 18:20:38 +0200

    chore: add dynamic formatting for biome and prettier

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/conform.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/conform.lua
@@ -1,20 +1,67 @@
-local prettier_with_lsp = { 'prettier', lsp_format = 'first' }
+local root_pattern = require('lspconfig.util').root_pattern
+
+local biome_or_prettier = function(bufnr)
+  local has_biome_config = root_pattern('biome.json', 'biome.jsonc')(vim.api.nvim_buf_get_name(bufnr))
+
+  if has_biome_config then
+    return { 'biome', lsp_format = 'fallback' }
+  end
+
+  local has_prettier_config = root_pattern(
+    '.prettierrc',
+    '.prettierrc.json',
+    '.prettierrc.yml',
+    '.prettierrc.yaml',
+    '.prettierrc.json5',
+    '.prettierrc.js',
+    '.prettierrc.cjs',
+    '.prettierrc.mjs',
+    '.prettierrc.toml',
+    'prettier.config.js',
+    'prettier.config.cjs',
+    'prettier.config.mjs'
+  )(bufnr)
+
+  if has_prettier_config then
+    return { 'prettier', lsp_format = 'first' }
+  end
+
+  return { 'prettier', lsp_format = 'first' }
+end
+
+local filetypes_with_dynamic_formatter = {
+  'astro',
+  'svelte',
+  'markdown',
+  'graphql',
+  'javascriptreact',
+  'typescriptreact',
+  'vue',
+  'javascript',
+  'json',
+  'jsonc',
+  'markdown.mdx',
+  'handlebars',
+  'typescript',
+  'yaml',
+  'css',
+  'less',
+  'scss',
+  'html',
+  'eruby',
+}
 
 ---@module "lazy"
 ---@type LazySpec[]
 return {
   {
     'stevearc/conform.nvim',
-    ---@type conform.setupOpts
-    opts = {
-      formatters_by_ft = {
-        ruby = { vim.g.lazyvim_ruby_formatter },
-        eruby = { 'prettier' },
-        javascript = prettier_with_lsp,
-        javascriptreact = prettier_with_lsp,
-        typescript = prettier_with_lsp,
-        typescriptreact = prettier_with_lsp,
-      },
-    },
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.ruby = { vim.g.lazyvim_ruby_formatter }
+      for _, ft in ipairs(filetypes_with_dynamic_formatter) do
+        opts.formatters_by_ft[ft] = biome_or_prettier
+      end
+    end,
   },
 }
--- home/dot_config/exact_nvim/exact_lua/exact_plugins/copilot.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/copilot.lua
@@ -9,10 +9,8 @@ return {
   },
   {
     'zbirenbaum/copilot.lua',
-    opts = function(_, opts)
-      opts.copilot_model = vim.g.copilot_model
-      -- Remove once https://github.com/LazyVim/LazyVim/pull/5900 is released
-      require('copilot.api').status = require('copilot.status')
-    end,
+    opts = {
+      copilot_model = vim.g.copilot_model,
+    },
   },
 }
