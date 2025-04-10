c9d34a2cc306c2dfd9ae0d0fbe75f09c97bcaa99
Miszo Radomski <hello@miszo.dev>
2025-04-10 16:56:22 +0200

    chore: remove wezterm types

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/lazydev.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/lazydev.lua
@@ -5,7 +5,6 @@ return {
     'folke/lazydev.nvim',
     dependencies = {
       { 'Bilal2453/luvit-meta', lazy = true },
-      { 'justinsgithub/wezterm-types', lazy = true },
     },
     ft = 'lua',
     cmd = 'LazyDev',
@@ -14,7 +13,6 @@ return {
         { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
         { path = '${3rd}/luassert/library', words = { 'assert' } },
         { path = '${3rd}/busted/library', words = { 'describe' } },
-        { path = 'wezterm-types', mods = { 'wezterm' } },
         { path = 'lazy.nvim', words = { 'LazySpec' } },
       })
     end,
