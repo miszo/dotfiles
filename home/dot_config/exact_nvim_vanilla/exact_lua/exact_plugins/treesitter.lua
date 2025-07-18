abdcf382efd53068f5f0f50e3a0020ad11a1f36c
Miszo Radomski <hello@miszo.dev>
2025-07-18 17:46:27 +0200

    chore: nvim vanilla config - treesitter

---
+++ home/dot_config/exact_nvim_vanilla/exact_lua/exact_plugins/treesitter.lua
@@ -0,0 +1,231 @@
+return {
+  { 'nvim-treesitter/playground', cmd = 'TSPlaygroundToggle' },
+
+  {
+    'folke/which-key.nvim',
+    opts = {
+      spec = {
+        { '<BS>', desc = 'Decrement Selection', mode = 'x' },
+        { '<c-space>', desc = 'Increment Selection', mode = { 'x', 'n' } },
+      },
+    },
+  },
+  -- Treesitter is a new parser generator tool that we can
+  -- use in Neovim to power faster and more accurate
+  -- syntax highlighting.
+  {
+    'nvim-treesitter/nvim-treesitter',
+    version = false, -- last release is way too old and doesn't work on Windows
+    build = function()
+      require('nvim-treesitter.install').update({ with_sync = true })()
+    end,
+    dependencies = {
+      'windwp/nvim-ts-autotag',
+      'RRethy/nvim-treesitter-endwise',
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      'nvim-treesitter/nvim-treesitter-context',
+      'LiadOz/nvim-dap-repl-highlights',
+    },
+    event = { 'BufReadPost', 'BufNewFile', 'BufWritePre', 'VeryLazy' },
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+    init = function(plugin)
+      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+      -- Luckily, the only things that those plugins need are the custom queries, which we make available
+      -- during startup.
+      require('lazy.core.loader').add_to_rtp(plugin)
+      require('nvim-treesitter.query_predicates')
+    end,
+    cmd = { 'TSUpdateSync', 'TSUpdate', 'TSInstall' },
+    keys = {
+      { '<c-space>', desc = 'Increment Selection' },
+      { '<bs>', desc = 'Decrement Selection', mode = 'x' },
+    },
+    ---@type TSConfig
+    ---@diagnostic disable-next-line: missing-fields
+    opts = {
+      autoinstall = true,
+      sync_install = false,
+      highlight = { enable = true, additional_vim_regex_highlighting = false },
+      indent = { enable = true },
+      ensure_installed = {
+        'angular',
+        'bash',
+        'blade',
+        'c',
+        'cpp',
+        'css',
+        'diff',
+        'dockerfile',
+        'gitignore',
+        'go',
+        'graphql',
+        'html',
+        'http',
+        'javascript',
+        'jsdoc',
+        'json',
+        'jsonc',
+        'lua',
+        'luadoc',
+        'luap',
+        'markdown',
+        'markdown_inline',
+        'mermaid',
+        'php',
+        'prisma',
+        'printf',
+        'python',
+        'query',
+        'regex',
+        'ruby',
+        'rust',
+        'scss',
+        'ssh_config',
+        'sql',
+        'svelte',
+        'swift',
+        'toml',
+        'tsx',
+        'typescript',
+        'vim',
+        'vimdoc',
+        'vue',
+        'xml',
+        'yaml',
+      },
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { 'BufWrite', 'CursorHold' },
+      },
+      playground = {
+        enable = true,
+        disable = {},
+        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+        persist_queries = true, -- Whether the query persists across vim sessions
+        keybindings = {
+          toggle_query_editor = 'o',
+          toggle_hl_groups = 'i',
+          toggle_injected_languages = 't',
+          toggle_anonymous_nodes = 'a',
+          toggle_language_display = 'I',
+          focus_language = 'f',
+          unfocus_language = 'F',
+          update = 'R',
+          goto_node = '<cr>',
+          show_help = '?',
+        },
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<C-space>',
+          node_incremental = '<C-space>',
+          scope_incremental = false,
+          node_decremental = '<bs>',
+        },
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ['af'] = { query = '@function.outer', desc = 'around a function' },
+            ['if'] = { query = '@function.inner', desc = 'inner part of a function' },
+            ['ac'] = { query = '@class.outer', desc = 'around a class' },
+            ['ic'] = { query = '@class.inner', desc = 'inner part of a class' },
+            ['ai'] = { query = '@conditional.outer', desc = 'around an if statement' },
+            ['ii'] = { query = '@conditional.inner', desc = 'inner part of an if statement' },
+            ['al'] = { query = '@loop.outer', desc = 'around a loop' },
+            ['il'] = { query = '@loop.inner', desc = 'inner part of a loop' },
+            ['ap'] = { query = '@parameter.outer', desc = 'around parameter' },
+            ['ip'] = { query = '@parameter.inner', desc = 'inside a parameter' },
+          },
+          selection_modes = {
+            ['@parameter.outer'] = 'v', -- charwise
+            ['@parameter.inner'] = 'v', -- charwise
+            ['@function.outer'] = 'v', -- charwise
+            ['@conditional.outer'] = 'V', -- linewise
+            ['@loop.outer'] = 'V', -- linewise
+            ['@class.outer'] = '<c-v>', -- blockwise
+          },
+          include_surrounding_whitespace = false,
+        },
+        move = {
+          enable = true,
+          goto_next_start = {
+            [']f'] = '@function.outer',
+            [']c'] = '@class.outer',
+            [']a'] = '@parameter.inner',
+          },
+          goto_next_end = {
+            [']F'] = '@function.outer',
+            [']C'] = '@class.outer',
+            [']A'] = '@parameter.inner',
+          },
+          goto_previous_start = {
+            ['[f'] = '@function.outer',
+            ['[c'] = '@class.outer',
+            ['[a'] = '@parameter.inner',
+          },
+          goto_previous_end = {
+            ['[F'] = '@function.outer',
+            ['[C'] = '@class.outer',
+            ['[A'] = '@parameter.inner',
+          },
+        },
+      },
+    },
+    ---@param opts TSConfig
+    config = function(_, opts)
+      local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+
+      parser_config.blade = {
+        install_info = {
+          url = 'https://github.com/EmranMR/tree-sitter-blade',
+          files = { 'src/parser.c' },
+          branch = 'main',
+        },
+        filetype = 'blade',
+      }
+
+      -- MDX
+      vim.filetype.add({
+        extension = {
+          mdx = 'mdx',
+        },
+        pattern = {
+          ['.*%.blade%.php'] = 'blade',
+        },
+      })
+      vim.treesitter.language.register('markdown', 'mdx')
+
+      require('nvim-treesitter.configs').setup(opts)
+    end,
+  },
+
+  -- Automatically add closing tags for HTML and JSX
+  {
+    'windwp/nvim-ts-autotag',
+    event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
+    opts = {},
+  },
+  {
+    'Wansmer/treesj',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      local tsj = require('treesj')
+      tsj.setup({
+        use_default_keymaps = false,
+      })
+      vim.keymap.set('n', '<leader>m', tsj.toggle, { desc = 'Toggle split/join code block' })
+    end,
+  },
+  -- Chezmoi syntax highlighting
+  {
+    'alker0/chezmoi.vim',
+  },
+}
---
+++ home/dot_config/exact_nvim_vanilla/init.lua
@@ -0,0 +1,11 @@
+require('core.mason-path')
+
+-- require("core.lsp")
+
+require('config.options')
+-- require("config.keymaps")
+-- require("config.autocmds")
+require('config.mason-verify')
+-- require("config.health-check")
+
+require('core.lazy')
