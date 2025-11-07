cff25a158892b1f01215290d79f88102776b5423
Miszo Radomski <hello@miszo.dev>
2025-11-07 11:49:22 +0100

    chore(nvim): add qol autocommands

--- home/dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
@@ -16,6 +16,45 @@ vim.api.nvim_create_autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
   end,
 })
 
+-- restore cursor to file position in previous editing session
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = augroup('restore_cursor_position'),
+  callback = function(args)
+    local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+    local line_count = vim.api.nvim_buf_line_count(args.buf)
+    if mark[1] > 0 and mark[1] <= line_count then
+      vim.api.nvim_win_set_cursor(0, mark)
+      -- defer centering slightly so it's applied after render
+      vim.schedule(function()
+        vim.cmd('normal! zz')
+      end)
+    end
+  end,
+})
+
+local group_active_cursorline = augroup('active_cursorline')
+
+-- highlight current line only in active window
+vim.api.nvim_create_autocmd({ 'WinEnter', 'BufEnter' }, {
+  group = group_active_cursorline,
+  callback = function()
+    vim.opt_local.cursorline = true
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'WinLeave', 'BufLeave' }, {
+  group = group_active_cursorline,
+  callback = function()
+    vim.opt_local.cursorline = false
+  end,
+})
+
+-- open help in vertical split
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'help',
+  command = 'wincmd L',
+})
+
 -- Highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = augroup('highlight_yank'),
@@ -31,6 +70,23 @@ vim.api.nvim_create_autocmd({ 'VimResized' }, {
     local current_tab = vim.fn.tabpagenr()
     vim.cmd('tabdo wincmd =')
     vim.cmd('tabnext ' .. current_tab)
+  end,
+})
+
+-- no auto continue comments on new line
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup('no_auto_comment'),
+  callback = function()
+    vim.opt_local.formatoptions:remove({ 'c', 'r', 'o' })
+  end,
+})
+
+-- syntax highlighting for dotenv files
+vim.api.nvim_create_autocmd('BufRead', {
+  group = augroup('dotenv_ft'),
+  pattern = { '.env', '.env.*' },
+  callback = function()
+    vim.bo.filetype = 'dosini'
   end,
 })
 
