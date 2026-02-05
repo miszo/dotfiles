08b88bd7e483b2db6e9624d88140524204884519
Miszo Radomski <hello@miszo.dev>
2026-02-05 14:21:17 +0100

    chore: adjust text search on dashboard

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/snacks.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/snacks.lua
@@ -19,7 +19,7 @@ local dashboard = {
     keys = {
       { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
       { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
-      { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
+      { icon = ' ', key = 't', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
       { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
       { icon = ' ', key = 's', desc = 'Restore Session', section = 'session' },
       {
