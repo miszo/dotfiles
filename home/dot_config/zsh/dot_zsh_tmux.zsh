9925121c8f8ff76589b1f5b4b9902880a4198b09
Miszo Radomski <hello@miszo.dev>
2026-03-31 02:40:00 +0200

    chore: better startup script

--- home/dot_config/zsh/dot_zsh_tmux.zsh
+++ home/dot_config/zsh/dot_zsh_tmux.zsh
@@ -2,6 +2,11 @@ if [[ -z "$TMUX" && $- == *i* ]]; then
   if tmux has-session 2>/dev/null; then
     exec tmux attach-session
   else
-    exec sesh connect "default"
+    # Avoid calling `sesh` by default because it may fail with "No connection found for default".
+    # If you explicitly want to use `sesh`, set `USE_SESH=1` in your environment.
+    if command -v sesh >/dev/null 2>&1 && [[ -n "${USE_SESH:-}" ]]; then
+      exec sesh connect "default"
+    fi
+    exec tmux new-session -s default
   fi
 fi
