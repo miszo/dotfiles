58305eca85c153984ec11d29bd31ddeb3f9a039c
Miszo Radomski <hello@miszo.dev>
2026-08-10 12:02:12 +0200

    chore(nvim): remove plugins

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/typescript.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/typescript.lua
@@ -15,23 +15,6 @@ return {
     },
   },
   {
-    'dmmulroy/tsc.nvim',
-    lazy = true,
-    ft = { 'typescript', 'typescriptreact' },
-    cmd = 'TSC',
-    keys = {
-      { '<leader>tc', '<cmd>TSC<cr>', desc = 'Type-check' },
-    },
-    config = function()
-      require('tsc').setup({
-        auto_open_qflist = true,
-        pretty_errors = false,
-        ---@diagnostic disable-next-line: assign-type-mismatch
-        flags = '--noEmit --pretty false', -- This just works
-      })
-    end,
-  },
-  {
     'typed-rocks/ts-worksheet-neovim',
     config = function(_, opts)
       require('tsw').setup(opts)
